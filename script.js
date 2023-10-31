687ded42c027e77760da27b2e00ef339e6e9280e
Connor Hill <119819818+hilcon3@users.noreply.github.com>
2023-10-31 19:32:22 -0400

    practice with functions

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // variables
+function add7(n){
+  return n+7;
+}
 
+function multiply(x, y){
+  return x*y;
+}
 
+function capitalize(str){
+  s = str.toLowerCase();
+  return s.substring(0,1).toUpperCase() + s.substring(1);
+}
+
+function lastLetter(str){
+  return str.charAt(str.length-1);
+}
+
+console.log(lastLetter("abcd"));
 
 
