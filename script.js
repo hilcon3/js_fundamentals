b949c66b0fcfd80cd1fbf0f1defdb68d419a50c8
Connor Hill <119819818+hilcon3@users.noreply.github.com>
2023-11-05 19:19:46 -0500

    practicing with DOM

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const container = document.querySelector('#container');
 
-const myarr = [1,2,3];
-console.log(myarr.toString());
+const p1 = document.createElement('p');
+p1.textContent = 'Hey I\'m red!';
+p1.style.color = 'red';
+container.appendChild(p1);
+
+const heading1 = document.createElement('h3');
+heading1.textContent = 'I\'m a blue h3!';
+heading1.style.color = 'blue';
+container.appendChild(heading1);
+
+const card = document.createElement('div');
+card.style.cssText = 'border: 4pd solid black;background-color: #FFC0CB';
+
+subheading = document.createElement('h1');
+subheading.textContent = 'I\'m in a div';
+para = document.createElement('p');
+para.textContent = 'Me TOO!';
+
+card.appendChild(subheading);
+card.appendChild(para);
+container.append(card);
+
